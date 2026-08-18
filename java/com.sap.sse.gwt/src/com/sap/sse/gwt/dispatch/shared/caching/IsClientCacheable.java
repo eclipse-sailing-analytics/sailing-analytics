a231c347fa4b38fa05a0f313af5f76a06f97afc3
Axel Uhl <axel.uhl@sap.com>
2026-08-18 14:09:26 +0200

    bug5429: tidy IsClientCacheable Javadoc
    Reflow the class/method comment and drop the empty @return tag; no code change.

--- java/com.sap.sse.gwt/src/com/sap/sse/gwt/dispatch/shared/caching/IsClientCacheable.java
+++ java/com.sap.sse.gwt/src/com/sap/sse/gwt/dispatch/shared/caching/IsClientCacheable.java
@@ -1,18 +1,14 @@
 package com.sap.sse.gwt.dispatch.shared.caching;
 
 /**
- * Cache interface used by the caching dispatch to identify which command can be cached on client side.
- * 
- * Each command must provide an instance key used to store the results in the cache.
- * 
- * The instance key is appended to an class identifying key.
+ * Cache interface used by the caching dispatch to identify which command can be cached on client side. Each command
+ * must provide an instance key used to store the results in the cache. The instance key is appended to an class
+ * identifying key.
  */
 public interface IsClientCacheable {
 
     /**
      * The builder used to identify instance specific results in the cache.
-     * 
-     * @return
      */
     void cacheInstanceKey(StringBuilder key);
 }
